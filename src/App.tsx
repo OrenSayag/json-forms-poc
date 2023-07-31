934404397cd509f1af53300346147460ce2b35b7
Oren Sayag <oren.s@techmarketing.co.il>
2023-07-31 08:58:57 +0300

    Init - JSON Forms basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { person } from '@jsonforms/examples';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import {JsonForms} from "@jsonforms/react";
+
+const schema = person.schema;
+const uischema = person.uischema;
+const initialData = person.data;
+
 function App() {
+  const [data, setData] = useState(initialData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App'>
+        <JsonForms
+            schema={schema}
+            uischema={uischema}
+            data={data}
+            renderers={materialRenderers}
+            cells={materialCells}
+            onChange={({ data, errors }) => setData(data)}
+        />
+      </div>
     </div>
   );
 }
